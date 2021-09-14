6860730432572a6cfa390b393577483a2f1bd75e
Lucas LESS. Eduardo dos Santos Silva <lucas@prosa.com>
2021-09-14 11:31:59 -0300

    alteracao de nome

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -3,7 +3,7 @@
 
 <head>
 
-    <title>Konsist</title>
+    <title>PROSA|Agendamentos</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
